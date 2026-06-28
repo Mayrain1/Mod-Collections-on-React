0013f14a816f3acd397b54c47a2c328933aeff50
Mayrain <ratmirmaznitsyn@gmail.com>
2026-06-28 12:38:50 +0500

    Add search for collections, add form instead of input in home.jsx

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import {Link} from "react-router-dom";
 
 function Collection({collection, removeCollection}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editForm, setEditForm] = useState(null);
     return (
         <li>
+            {isEditing ? (<>
+
+            </>)
+            : (
+            <>
             <Link to={`/collection/${collection.id}`}>{collection.title}</Link>
+            <p>{collection.description}</p>
             <button onClick={() => removeCollection(collection.id)}>Удалить</button>
+            <button onClick={() => {
+                setEditForm({...collection})
+                setIsEditing(true)
+            }}>Редактировать</button>
+            </>)}
         </li>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,38 +1,73 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import Collection from "../components/Collection";
 
 function Home({ collections, setCollections }) {
-  const [inputValue, setInputValue] = useState("");
+  const [collectionForm, setCollectionForm] = useState({
+    title: "",
+    description: ""
+  });
+
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addCollection();
+  }
+
+  function handleChange(e) {
+    setCollectionForm({...collectionForm, [e.target.name]: e.target.value});
+  }
 
   function addCollection() {
-    const value = inputValue.trim();
-    if (!value) return;
+    const hasEmptyField = Object.values(collectionForm).some(field => field.trim().length === 0);
+    if (hasEmptyField) return;
    
     const collection = {
       id: Date.now(),
-      title: value,
-      description: null,
+      title: collectionForm.title.trim(),
+      description: collectionForm.description.trim(),
       mods: []
     }
 
     setCollections([...collections, collection]);
-    setInputValue("");
+    setCollectionForm({
+      title: "",
+      description: ""
+    });
   }
 
   function removeCollection(id) {
     setCollections(collections.filter(collection => id !== collection.id));
   }
 
+  const filteredCollections = collections.filter(col => 
+    col.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <div className="container">
-      <label htmlFor="collection-title">Введите значение: </label>
-      <input id="collection-title"
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}/>
-      <button onClick={addCollection}>Добавить</button>
+      <label htmlFor="collection-search-input">Введите название чтобы найти коллекцию: </label>
+      <input id="collection-search-input"
+      value={searchValue}
+      onChange={(e) => setSearchValue(e.target.value)}/>
+
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="collection-title">Введите название коллекции: </label>
+          <input id="collection-title"
+          name="title"
+          value={collectionForm.title}
+          onChange={handleChange}/>
+
+           <label htmlFor="collection-title">Введите описание коллекции: </label>
+          <input id="collection-description"
+          name="description"
+          value={collectionForm.description}
+          onChange={handleChange}/>
+          <button onClick={addCollection}>Добавить коллекцию</button>
+      </form>
 
       <ul className="collections-list">
-        {collections.map(collection => (
+        {filteredCollections.map(collection => (
           <Collection 
           key={collection.id}
           collection={collection}
